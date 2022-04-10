30184e675763e0359a2d0b6774e139602e8b9721
akmalali17 <103355300+akmalali17@users.noreply.github.com>
2022-04-10 17:08:31 +0700

    Create soal4.R

---
+++ soal4.R
@@ -0,0 +1,25 @@
+#soal no 4
+
+#a
+x = 2
+df = 10
+dchisq(x , df)
+
+#b
+p <- rchisq(100,df)
+
+hist(p,
+     freq = FALSE,
+     xlim = c(0,30),
+     ylim = c(0,0.2),
+     main = "Histogram Of Chisquare")
+
+#c
+#mean
+mean1 = df
+mean1
+
+#variance
+var1 = 2*df
+var1
+
